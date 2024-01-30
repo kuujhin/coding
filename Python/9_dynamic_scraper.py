e966bbcf2e8a1866bb122bdcea0b1a72867447c7
kuujhin <kdoubleu0711@gmail.com>
2024-01-30 23:58:23 +0900

    exporting to excel

--- Python/9_dynamic_scraper.py
+++ Python/9_dynamic_scraper.py
@@ -11,6 +11,8 @@
 from playwright.sync_api import sync_playwright
 import time
 from bs4 import BeautifulSoup
+import csv
+
 p = sync_playwright().start()
 
 browser = p.chromium.launch(headless=False)
@@ -43,8 +45,8 @@ page = browser.new_page()
 page.goto("https://www.wanted.co.kr/search?query=flutter&tab=position")
 
 for x in range(4):
-  page.keyboard.down("End")
-  time.sleep(3)
+    page.keyboard.down("End")
+    time.sleep(3)
 
 content = page.content()
 
@@ -57,21 +59,31 @@ jobs = soup.find_all("div", class_="JobCard_container__FqChn")
 jobs_db = []
 
 for job in jobs:
-  link = f"https://www.wanted.co.kr{job.find('a')['href']}"
-  title = job.find("strong", class_="JobCard_title__ddkwM").text
-  company_name = job.find("span", class_="JobCard_companyName__vZMqJ").text
-  location = job.find("span", class_="JobCard_location__2EOr5").text
-  reword = job.find("span", class_="JobCard_reward__sdyHn").text
+    link = f"https://www.wanted.co.kr{job.find('a')['href']}"
+    title = job.find("strong", class_="JobCard_title__ddkwM").text
+    company_name = job.find("span", class_="JobCard_companyName__vZMqJ").text
+    location = job.find("span", class_="JobCard_location__2EOr5").text
+    reword = job.find("span", class_="JobCard_reward__sdyHn").text
 
-  job = {
-    "title": title,
-    "company_name": company_name,
-    "location": location,
-    "reword": reword,
-    "link": link,
-  }
+    job = {
+        "title": title,
+        "company_name": company_name,
+        "location": location,
+        "reword": reword,
+        "link": link,
+    }
 
-  jobs_db.append(job)
+    jobs_db.append(job)
 
-print(jobs_db)
-print(len(jobs_db))
+# CSV : Comma Separated Values
+file = open("./Python/jobs.csv", "w", encoding="UTF-8", newline="")
+writer = csv.writer(file)
+writer.writerow(["Title", "Company", "Location", "Reward", "Link"])
+
+# # Values from Dictionary
+# a = {'a': 1, 'b': 2, 'c': 3}
+# a.values()  # >>> dict_values([1,2,3])
+# a.keys()    # >>> dict_keys(['a','b','c'])
+
+for job in jobs_db:
+    writer.writerow(job.values())
