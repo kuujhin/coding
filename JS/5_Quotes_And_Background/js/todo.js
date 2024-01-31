f0dffb6eb32147702416129dd8fe8b4773a8dc25
kuujhin <kdoubleu0711@gmail.com>
2024-01-31 20:12:45 +0900

    deleting todos

--- JS/5_Quotes_And_Background/js/todo.js
+++ JS/5_Quotes_And_Background/js/todo.js
@@ -3,11 +3,22 @@ const toDoInput = toDoForm.querySelector("input");
 //const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
 function paintToDo(newToDo) {
   const li = document.createElement("li");
+
   const span = document.createElement("span");
-  li.appendChild(span);
   span.innerText = newToDo;
+
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
   toDoList.appendChild(li);
 }
 
@@ -15,7 +26,6 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newToDo = toDoInput.value;
   toDoInput.value = "";
-  console.log(newToDo);
   paintToDo(newToDo);
 }
 
