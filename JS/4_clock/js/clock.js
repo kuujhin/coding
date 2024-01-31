50cb43c0dfe755b5f89728857d0b1605be1adf0e
kuujhin <kdoubleu0711@gmail.com>
2024-01-31 12:16:48 +0900

    PadStart

--- JS/4_clock/js/clock.js
+++ JS/4_clock/js/clock.js
@@ -14,12 +14,26 @@
 // date.getMinutes()
 // date.getSeconds()
 
+// // padStart(a, b)
+// // 길이 a인 string이 되도록 앞쪽에 b로 채움
+// console.log("1".padStart(2, "0"))
+// console.log("12".padStart(2, "0"))
+// console.log("hello".padStart(20,"x"))
+
+// // numberToString
+// console.log(1);
+// console.log(String(1));
+
 const clock = document.querySelector("h2#clock");
 
 function getClock() {
-  const date = new Date()
-  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
+  // clock.innerText = date.toLocaleTimeString("en-US", { hour12: false });
 }
 
-getClock()
+getClock();
 setInterval(getClock, 1000);
