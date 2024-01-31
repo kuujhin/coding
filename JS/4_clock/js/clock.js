30f77d432ebad7dc357f447d716a825f92e4c6a3
kuujhin <kdoubleu0711@gmail.com>
2024-01-31 12:04:09 +0900

    timeouts and dates

--- JS/4_clock/js/clock.js
+++ JS/4_clock/js/clock.js
@@ -1,9 +1,25 @@
-const clock = document.querySelector("h2#clock");
-
-function sayHello() {
-  console.log("hello")
-}
-
-// 특정 시간마다 함수 실행
+// // 특정 시간마다 함수 실행
 // setInterval(sayHello, 5000)
 // 5000ms = 5s
+
+// // 특정 시간 이후 함수 실행
+// setTimeout(sayHello, 5000);
+
+// // Date Object
+// const date = new Date()
+// date.getDate()
+// date.getDay()
+// date.getFullYear()
+// date.getHours()
+// date.getMinutes()
+// date.getSeconds()
+
+const clock = document.querySelector("h2#clock");
+
+function getClock() {
+  const date = new Date()
+  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+}
+
+getClock()
+setInterval(getClock, 1000);
