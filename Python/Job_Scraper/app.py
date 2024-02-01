f675422d16e75d5f5b78ac05e200939576301d3e
kuujhin <kdoubleu0711@gmail.com>
2024-02-01 15:32:10 +0900

    using cache db

--- Python/Job_Scraper/app.py
+++ Python/Job_Scraper/app.py
@@ -3,6 +3,8 @@ from scraper import Jobs
 
 app = Flask(__name__)
 
+db = {}
+
 
 @app.route("/")
 def home():
@@ -12,10 +14,15 @@ def home():
 @app.route("/search")
 def search():
     keyword = request.args.get("keyword")
-    jobs_db = Jobs(keyword)
-    jobs_db.get_content()
-    jobs_db.scrape_page()
-    return render_template("search.html", keyword=keyword, jobs=jobs_db.jobs)
+    if keyword in db:
+        jobs = db[keyword]
+    else:
+        jobs_db = Jobs(keyword)
+        jobs_db.get_content()
+        jobs_db.scrape_page()
+        jobs = jobs_db.jobs
+        db[keyword] = jobs
+    return render_template("search.html", keyword=keyword, jobs=jobs)
 
 
 app.run("0.0.0.0", debug=True)
