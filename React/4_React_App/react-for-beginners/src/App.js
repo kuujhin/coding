eedbbba3a33e19e0f4ee0b051bb49eba498c0ad3
kuujhin <kdoubleu0711@gmail.com>
2024-02-02 19:10:36 +0900

    useEffect

--- React/4_React_App/react-for-beginners/src/App.js
+++ React/4_React_App/react-for-beginners/src/App.js
@@ -1,11 +1,14 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Button from "./Button";
 import styles from "./App.module.css";
 
 function App() {
   const [counter, setCounter] = useState(0);
   const onClick = () => setCounter((prev) => prev + 1);
-  console.log("render");
+  console.log("I run all the time");
+  useEffect(() => {
+    console.log("call API");
+  }, []);
   return (
     <div>
       <h1 className={styles.title}>{counter}</h1>
