61a3d4b0701f2de34c41a622bbfa261256f7f564
kuujhin <kdoubleu0711@gmail.com>
2024-02-06 15:38:43 +0900

    server side render

--- Next.js/learn-nextjs14/components/navigation.tsx
+++ Next.js/learn-nextjs14/components/navigation.tsx
@@ -1,4 +1,7 @@
+// Server Side Render vs. Client Side Render
+// Backend에서 먼저 컴포넌트와 페이지가 render된 후 JS를 적용한 html을 보내줌
 "use client";
+
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
