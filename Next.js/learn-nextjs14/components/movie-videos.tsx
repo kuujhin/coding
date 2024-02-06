c47a8b37db2781e4dda4971fdb08976e5f764e12
kuujhin <kdoubleu0711@gmail.com>
2024-02-06 21:24:24 +0900

    error handling

--- Next.js/learn-nextjs14/components/movie-videos.tsx
+++ Next.js/learn-nextjs14/components/movie-videos.tsx
@@ -2,8 +2,9 @@ import { API_URL } from "../app/(home)/page";
 
 async function getVideos(id: string) {
   await new Promise((resolve) => setTimeout(resolve, 3000));
-  const response = await fetch(`${API_URL}/${id}/videos`);
-  return response.json();
+  //   const response = await fetch(`${API_URL}/${id}/videos`);
+  //   return response.json();
+  throw new Error("something broke...");
 }
 
 export default async function MovieVideos({ id }: { id: string }) {
