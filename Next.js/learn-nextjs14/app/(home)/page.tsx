b509b69b0f706f8c99ade92b5608ad3d5a9546a9
kuujhin <kdoubleu0711@gmail.com>
2024-02-06 20:07:15 +0900

    loading components

--- Next.js/learn-nextjs14/app/(home)/page.tsx
+++ Next.js/learn-nextjs14/app/(home)/page.tsx
@@ -1,5 +1,7 @@
 // Server Component Page
 
+import Link from "next/link";
+
 export const metadata = {
   title: "Home",
 };
@@ -7,8 +9,8 @@ export const metadata = {
 const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
-  //로딩 상태를 확인하기 위해 5초 기다리는 코드
-  await new Promise((resolve) => setTimeout(resolve, 5000));
+  //로딩 상태를 확인하기 위해 기다리는 코드
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   // fetch(URL).then((response) => response.json());
   const response = await fetch(URL);
   const json = await response.json();
@@ -17,8 +19,17 @@ async function getMovies() {
 
 export default async function HomePage() {
   const movies = await getMovies();
-  return <div>{JSON.stringify(movies)}</div>;
+  return (
+    <div>
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
+    </div>
+  );
 }
 
 // Network에 movie 가져오는 API를 볼 수 없다.
 // 데이터가 도착할 때 까지 다른 요소도 볼 수 없다.
+// UI가 이예 없다.랜더링이 안일어나기 때문
