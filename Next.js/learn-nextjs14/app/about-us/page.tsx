40cb264d55ba9335f6ab1d143d2d41196cffa4a5
kuujhin <kdoubleu0711@gmail.com>
2024-02-06 14:36:56 +0900

    navigation component

--- Next.js/learn-nextjs14/app/about-us/page.tsx
+++ Next.js/learn-nextjs14/app/about-us/page.tsx
@@ -1,12 +1,16 @@
 // /about-us 주소에 사용할 페이지
 // 반드시 폴더에 page 파일이 존재해야함
 
-import Avartar from "./components/avatar";
+import Navigation from "../../components/navigation";
+import Avartar from "../../components/avatar";
 
 export default function AboutUs() {
   return (
-    <h1>
-      I'm JHIN! <Avartar />
-    </h1>
+    <div>
+      <Navigation />
+      <h1>
+        I'm JHIN! <Avartar />
+      </h1>
+    </div>
   );
 }
