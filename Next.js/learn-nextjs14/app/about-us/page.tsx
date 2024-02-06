d923398350b754fb643bbea91e4f8536d5cff9d6
kuujhin <kdoubleu0711@gmail.com>
2024-02-06 14:12:04 +0900

    defining routes

---
+++ Next.js/learn-nextjs14/app/about-us/page.tsx
@@ -0,0 +1,12 @@
+// /about-us 주소에 사용할 페이지
+// 반드시 폴더에 page 파일이 존재해야함
+
+import Avartar from "./components/avatar";
+
+export default function AboutUs() {
+  return (
+    <h1>
+      I'm JHIN! <Avartar />
+    </h1>
+  );
+}
