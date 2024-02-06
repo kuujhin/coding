182a1abd2376887a4b3d5c501d1a978851eba005
kuujhin <kdoubleu0711@gmail.com>
2024-02-06 17:22:20 +0900

    layouts

--- Next.js/learn-nextjs14/app/about-us/page.tsx
+++ Next.js/learn-nextjs14/app/about-us/page.tsx
@@ -1,16 +1,13 @@
 // /about-us 주소에 사용할 페이지
 // 반드시 폴더에 page 파일이 존재해야함
 
-import Navigation from "../../components/navigation";
 import Avartar from "../../components/avatar";
 
 export default function AboutUs() {
   return (
     <div>
-      <Navigation />
-      <h1>
-        I'm JHIN! <Avartar />
-      </h1>
+      <h1>I'm JHIN!</h1>
+      <Avartar />
     </div>
   );
 }
--- Next.js/learn-nextjs14/app/not-found.tsx
+++ Next.js/learn-nextjs14/app/not-found.tsx
@@ -1,11 +1,8 @@
 // 잘못된 주소로 갔을 시 보여주는 리액트 컴포넌트
 
-import Navigation from "../components/navigation";
-
 export default function NotFound() {
   return (
     <div>
-      <Navigation />
       <h1>Not Found!</h1>
     </div>
   );
