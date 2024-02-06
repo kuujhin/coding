9e6604234e6b3de43128b78142abdd52495465a9
kuujhin <kdoubleu0711@gmail.com>
2024-02-06 19:51:18 +0900

    server side data fetching

--- Next.js/learn-nextjs14/app/(home)/page.tsx
+++ Next.js/learn-nextjs14/app/(home)/page.tsx
@@ -1,11 +1,24 @@
+// Server Component Page
+
 export const metadata = {
   title: "Home",
 };
 
-export default function Page() {
-  return (
-    <div>
-      <h1>Hello!</h1>
-    </div>
-  );
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+async function getMovies() {
+  //로딩 상태를 확인하기 위해 5초 기다리는 코드
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  // fetch(URL).then((response) => response.json());
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
 }
+
+export default async function HomePage() {
+  const movies = await getMovies();
+  return <div>{JSON.stringify(movies)}</div>;
+}
+
+// Network에 movie 가져오는 API를 볼 수 없다.
+// 데이터가 도착할 때 까지 다른 요소도 볼 수 없다.
