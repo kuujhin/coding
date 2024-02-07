4e51b537c6f61cf8842fc2197f8e7dc1a3ad7bf8
kuujhin <kdoubleu0711@gmail.com>
2024-02-07 16:38:36 +0900

    css modules & movie styles

--- Next.js/learn-nextjs14/app/(home)/page.tsx
+++ Next.js/learn-nextjs14/app/(home)/page.tsx
@@ -1,6 +1,8 @@
 // Server Component Page
 
 import Link from "next/link";
+import Movie from "../../components/movie";
+import styles from "../../styles/home.module.css";
 
 export const metadata = {
   title: "Home",
@@ -20,11 +22,14 @@ async function getMovies() {
 export default async function HomePage() {
   const movies = await getMovies();
   return (
-    <div>
+    <div className={styles.container}>
       {movies.map((movie) => (
-        <li key={movie.id}>
-          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
-        </li>
+        <Movie
+          key={movie.id}
+          id={movie.id}
+          poster_path={movie.poster_path}
+          title={movie.title}
+        />
       ))}
     </div>
   );
--- Next.js/learn-nextjs14/components/navigation.tsx
+++ Next.js/learn-nextjs14/components/navigation.tsx
@@ -23,12 +23,13 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
+import styles from "../styles/navigation.module.css";
 
 export default function Navigation() {
   const path = usePathname();
   const [count, setCount] = useState(0);
   return (
-    <nav>
+    <nav className={styles.nav}>
       <ul>
         <li>
           <Link href="/">Home</Link> {path === "/" ? "🔥" : ""}
@@ -36,9 +37,6 @@ export default function Navigation() {
         <li>
           <Link href="/about-us">About Us</Link>
           {path === "/about-us" ? "🔥" : ""}
-        </li>
-        <li>
-          <button onClick={() => setCount((c) => c + 1)}>{count}</button>
         </li>
       </ul>
     </nav>
