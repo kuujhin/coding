c2ceae0164b8449a31fd611a81c36a7c3fdc7eb3
kuujhin <kdoubleu0711@gmail.com>
2024-02-07 11:45:23 +0900

    defining function

---
+++ Dart/3_function.dart
@@ -0,0 +1,10 @@
+// String sayHello(String name) {
+//   return "Hello $name nice to meet you!";
+// }
+
+// fat arrow syntax
+String sayHello(String name) => "Hello $name nice to meet you!";
+
+void main() {
+  print(sayHello('jhin'));
+}
