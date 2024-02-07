7e88bc15324f4d6ec13d7aa1c929f0cadeca84f8
kuujhin <kdoubleu0711@gmail.com>
2024-02-07 14:47:25 +0900

    defining a function

--- Dart/3_function.dart
+++ Dart/3_function.dart
@@ -1,9 +1,13 @@
+/////////// 1. Defining a Function ///////////
+
 // String sayHello(String name) {
 //   return "Hello $name nice to meet you!";
 // }
 
 // fat arrow syntax
 String sayHello(String name) => "Hello $name nice to meet you!";
+
+num plus(num a, num b) => a + b;
 
 void main() {
   print(sayHello('jhin'));
