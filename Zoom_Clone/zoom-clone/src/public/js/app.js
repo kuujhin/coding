163baaa20f2850b9ef0be973ae2f0f9a4593e041
kuujhin <kdoubleu0711@gmail.com>
2024-02-14 16:54:02 +0900

    nicknames & left room

--- Zoom_Clone/zoom-clone/src/public/js/app.js
+++ Zoom_Clone/zoom-clone/src/public/js/app.js
@@ -3,6 +3,7 @@ const socket = io();
 const welcome = document.getElementById("welcome");
 const form = welcome.querySelector("form");
 const room = document.getElementById("room");
+const left = document.querySelector("#left button");
 
 room.hidden = true;
 
@@ -15,9 +16,14 @@ function addMessage(msg) {
   ul.appendChild(li);
 }
 
+function deleteMessage() {
+  const ul = room.querySelector("ul");
+  while (ul.hasChildNodes()) ul.removeChild(ul.firstChild);
+}
+
 function handleMessageSubmit(event) {
   event.preventDefault();
-  const input = room.querySelector("input");
+  const input = room.querySelector("#msg input");
   const value = input.value;
   socket.emit("new_message", value, roomName, () => {
     addMessage(`You: ${value}`);
@@ -25,27 +31,46 @@ function handleMessageSubmit(event) {
   input.value = "";
 }
 
+// function handleNicknameSubmit(event) {
+//   event.preventDefault();
+//   const input = room.querySelector("#name input");
+//   const value = input.value;
+//   socket.emit("nickname", value);
+//   input.value = "";
+// }
+
 function showRoom() {
   welcome.hidden = true;
   room.hidden = false;
   const h3 = room.querySelector("h3");
   h3.innerText = `Room ${roomName}`;
-  const form = room.querySelector("form");
-  form.addEventListener("submit", handleMessageSubmit);
+  const msgForm = room.querySelector("#msg");
+  msgForm.addEventListener("submit", handleMessageSubmit);
 }
 
 function handleRoomSubmit(event) {
   event.preventDefault();
-  const input = form.querySelector("input");
-  socket.emit("enter_room", input.value, showRoom);
-  roomName = input.value;
-  input.value = "";
+  const roominput = form.querySelector("#roomname");
+  const nameinput = form.querySelector("#nickname");
+  roomName = roominput.value;
+  const nickName = nameinput.value;
+  socket.emit("enter_room", roomName, nickName, showRoom);
+  roominput.value = "";
+  nameinput.value = "";
+}
+
+function handleRoomLeft() {
+  welcome.hidden = false;
+  room.hidden = true;
+  socket.emit("left_room", roomName, deleteMessage);
 }
 
 form.addEventListener("submit", handleRoomSubmit);
 
-socket.on("welcome", () => addMessage("Someone Joined!"));
+left.addEventListener("click", handleRoomLeft);
+
+socket.on("welcome", (user) => addMessage(`${user} Joined!`));
 
 socket.on("new_message", addMessage);
 
-socket.on("bye", () => addMessage("Someone Left!"));
+socket.on("bye", (user) => addMessage(`${user} Left!`));
--- Zoom_Clone/zoom-clone/src/server.js
+++ Zoom_Clone/zoom-clone/src/server.js
@@ -19,21 +19,31 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
+  socket["nickname"] = "Anon";
   socket.onAny((event) => {
     console.log(`Socket Event: ${event}`);
   });
-  socket.on("enter_room", (roomName, done) => {
+  socket.on("enter_room", (roomName, nickName, done) => {
+    if (nickName !== "") {
+      socket["nickname"] = nickName;
+    }
     socket.join(roomName);
     done();
-    socket.to(roomName).emit("welcome");
+    socket.to(roomName).emit("welcome", socket.nickname);
   });
   socket.on("new_message", (msg, room, done) => {
-    socket.to(room).emit("new_message", msg);
+    socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
+    done();
+  });
+  socket.on("left_room", (roomName, done) => {
+    socket.leave(roomName);
+    socket.to(roomName).emit("bye", socket.nickname);
+    socket["nickname"] = "Anon";
     done();
   });
   socket.on("disconnecting", () => {
     socket.rooms.forEach((room) => {
-      socket.to(room).emit("bye");
+      socket.to(room).emit("bye", socket.nickname);
     });
   });
 });
