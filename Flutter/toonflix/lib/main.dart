076c5b82fbf88efc0a747db1e2a6b282109cf46c
kuujhin <kdoubleu0711@gmail.com>
2024-02-15 21:04:30 +0900

    BuildContext

--- Flutter/toonflix/lib/main.dart
+++ Flutter/toonflix/lib/main.dart
@@ -13,53 +13,43 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  List<int> numbers = [];
-
-  void addCount() {
-    setState(() {
-      numbers.add(numbers.length);
-    });
-  }
-
-  void reset() {
-    setState(() {
-      numbers = [];
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: const Color(0xFFF4EDDB),
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red,
+          ),
+        ),
+      ),
+      home: const Scaffold(
+        backgroundColor: Color(0xFFF4EDDB),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text(
-                'Click Count',
-                style: TextStyle(
-                  fontSize: 30,
-                ),
-              ),
-              for (var n in numbers) Text('$n'),
-              IconButton(
-                iconSize: 40,
-                onPressed: addCount,
-                icon: const Icon(
-                  Icons.add_box_rounded,
-                ),
-              ),
-              IconButton(
-                iconSize: 40,
-                onPressed: reset,
-                icon: const Icon(
-                  Icons.refresh_sharp,
-                ),
-              ),
+              MyLargeTitle(),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class MyLargeTitle extends StatelessWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'My Large Title',
+      style: TextStyle(
+        fontSize: 30,
+        color: Theme.of(context).textTheme.titleLarge?.color,
       ),
     );
   }
