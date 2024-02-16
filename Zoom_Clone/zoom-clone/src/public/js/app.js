0931b026157a753813d395fd66b00b268f559569
kuujhin <kdoubleu0711@gmail.com>
2024-02-16 13:17:46 +0900

    rooms & offers

--- Zoom_Clone/zoom-clone/src/public/js/app.js
+++ Zoom_Clone/zoom-clone/src/public/js/app.js
@@ -5,19 +5,31 @@ const micButton = document.getElementById("mic");
 const cameraButton = document.getElementById("camera");
 const camerasSelect = document.getElementById("cameras");
 
+const call = document.getElementById("call");
+
+call.hidden = true;
+
 let myStream;
 
 let micOff = false;
 let cameraOff = false;
 
+let roomName;
+
+let myPeerConnection;
+
 async function getCameras() {
   try {
     const devices = await navigator.mediaDevices.enumerateDevices();
     const cameras = devices.filter((device) => device.kind === "videoinput");
+    const currentCamera = myStream.getVideoTracks()[0];
     cameras.forEach((camera) => {
       const option = document.createElement("option");
       option.value = camera.deviceId;
       option.innerText = camera.label;
+      if (currentCamera.label == camera.label) {
+        option.selected = true;
+      }
       camerasSelect.appendChild(option);
     });
   } catch (error) {
@@ -25,20 +37,25 @@ async function getCameras() {
   }
 }
 
-async function getMedia() {
+async function getMedia(deviceId) {
+  const initialConstrains = {
+    audio: true,
+    video: { facingMode: "user" },
+  };
+  const cameraConstrains = {
+    audio: true,
+    video: { deviceId: deviceId },
+  };
   try {
-    myStream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: true,
-    });
+    myStream = await navigator.mediaDevices.getUserMedia(
+      deviceId ? cameraConstrains : initialConstrains
+    );
     myFace.srcObject = myStream;
-    await getCameras();
+    if (!deviceId) await getCameras();
   } catch (error) {
     console.log(error);
   }
 }
-
-getMedia();
 
 function handleMicClick() {
   myStream.getAudioTracks().forEach((track) => {
@@ -64,5 +81,59 @@ function handleCameraClick() {
   cameraOff = !cameraOff;
 }
 
+async function handelCameraChange() {
+  await getMedia(camerasSelect.value);
+  if (micOff) {
+    myStream.getAudioTracks().forEach((track) => (track.enabled = false));
+  } else {
+    myStream.getAudioTracks().forEach((track) => (track.enabled = true));
+  }
+}
+
 cameraButton.addEventListener("click", handleCameraClick);
 micButton.addEventListener("click", handleMicClick);
+camerasSelect.addEventListener("input", handelCameraChange);
+
+//Welcome Form (join a room)
+
+const welcome = document.getElementById("welcome");
+const welcomeForm = welcome.querySelector("form");
+
+async function startMedia() {
+  welcome.hidden = true;
+  call.hidden = false;
+  await getMedia();
+  makeConnection();
+}
+
+function handleWelcomeSubmit(event) {
+  event.preventDefault();
+  const input = welcomeForm.querySelector("input");
+  socket.emit("join_room", input.value, startMedia);
+  roomName = input.value;
+  input.value = "";
+}
+
+welcomeForm.addEventListener("submit", handleWelcomeSubmit);
+
+// Socket Code
+
+socket.on("welcome", async () => {
+  const offer = await myPeerConnection.createOffer();
+  myPeerConnection.setLocalDescription(offer);
+  console.log("sent the offer");
+  socket.emit("offer", offer, roomName);
+});
+
+socket.on("offer", (offer) => {
+  console.log(offer);
+});
+
+// RTC Code
+
+function makeConnection() {
+  myPeerConnection = new RTCPeerConnection();
+  myStream
+    .getTracks()
+    .forEach((track) => myPeerConnection.addTrack(track, myStream));
+}
