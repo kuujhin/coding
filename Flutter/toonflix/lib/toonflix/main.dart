8586dd3cc27ece11d06a705e0bcaf3a823e2a970
kuujhin <kdoubleu0711@gmail.com>
2024-02-18 21:31:55 +0900

    data fetching

--- Flutter/toonflix/lib/toonflix/main.dart
+++ Flutter/toonflix/lib/toonflix/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:toonflix/toonflix/screens/home_screen.dart';
+import 'package:toonflix/toonflix/services/api_service.dart';
 
 void main() {
+  ApiService().getTodaysToons();
   runApp(const App());
 }
 
