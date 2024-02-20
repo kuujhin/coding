d11562f9c1278ee3f043c2249c6d51ccdab388e1
kuujhin <kdoubleu0711@gmail.com>
2024-02-20 12:27:54 +0900

    polymorphism

--- TS/3_classes_and_interfaces.ts
+++ TS/3_classes_and_interfaces.ts
@@ -196,3 +196,35 @@ makeUser({
   fullName: () => "xx",
   sayHi: (name) => "string",
 });
+
+///////////3. Polymorphism
+interface SStorage<T> {
+  [key: string]: T;
+}
+
+class LocalStorage<T> {
+  private storage: SStorage<T> = {};
+
+  set(key: string, value: T) {
+    this.storage[key] = value;
+  }
+  remove(key: string) {
+    delete this.storage[key];
+  }
+  get(key: string): T {
+    return this.storage[key];
+  }
+  clear() {
+    this.storage = {};
+  }
+}
+
+const stringsStorage = new LocalStorage<string>();
+
+stringsStorage.get("key");
+stringsStorage.set("hello", "how r u");
+
+const booleansStorage = new LocalStorage<boolean>();
+
+booleansStorage.get("xxx");
+booleansStorage.set("hello", true);
